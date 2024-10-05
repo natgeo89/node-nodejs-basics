2b92f24eb8b8124c60581730de845bdcdacda3f5
name name <wnwaycha@gmail.com>
2024-10-05 19:13:42 +0300

    child processes task

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,6 +1,18 @@
+import { fork } from "child_process";
+import path from "node:path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const scriptFilePath = path.resolve(__dirname, "files", "script.js");
+
 const spawnChildProcess = async (args) => {
-    // Write your code here
+  const childProcess = fork(scriptFilePath, args);
+
+  childProcess.on("message", (msg) => {
+    console.log("Message from child", msg);
+  });
 };
 
 // Put your arguments in function call to test this functionality
-spawnChildProcess( /* [someArgument1, someArgument2, ...] */);
+spawnChildProcess([10, "sometext"]);
