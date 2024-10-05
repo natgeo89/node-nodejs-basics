52296367cf93f531e89fdfd73908b335be7ecb29
name name <wnwaycha@gmail.com>
2024-10-05 10:46:06 +0300

    zlib tasks

--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,22 @@
+import { createReadStream, createWriteStream } from "node:fs";
+import path from "node:path";
+import zlib from "node:zlib";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const decompress = async () => {
-    // Write your code here 
+  const fileToDecompressPath = path.resolve(__dirname, "files", "archive.gz");
+  const decompressedFilePath = path.resolve(__dirname, "files", "decompressedFile.txt");
+
+
+  const unzip = zlib.createUnzip();
+
+  const readStream = createReadStream(fileToDecompressPath);
+  const writeStream = createWriteStream(decompressedFilePath);
+
+  readStream.pipe(unzip).pipe(writeStream);	
 };
 
 await decompress();
